e549c12d95bce6b08c7c5d148bc124a7b5af74f2
RomanBezsh <antikys189@gmail.com>
2026-01-04 18:26:08 +0200

    innit commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthPage } from "./pages/Auth/AuthPage.jsx";
 import { RegisterPage } from "./pages/Register/RegisterPage.jsx";
 import { Dashboard } from "./pages/Dashboard/Dashboard.jsx";
+import { Notifications } from "./pages/Notifications/Notifications.jsx";
 import { Layout } from "./components/Layout/Layout.jsx";
 
 const App = () => {
@@ -10,14 +11,13 @@ const App = () => {
         <BrowserRouter>
             <div className="App">
                 <Routes>
-                    /* Страницы БЕЗ хедера */
                     <Route path="/" element={<AuthPage />} />
                     <Route path="/sign-up" element={<RegisterPage />} />
 
-                    /* Страницы С хедером */
+
                     <Route element={<Layout />}>
                         <Route path="/dashboard" element={<Dashboard />} />
-
+                        <Route path="/notifications" element={<Notifications />} />
                     </Route>
                 </Routes>
             </div>
---
+++ src/pages/Notifications/Notifications.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+export const Notifications = () => {
+    return (
+        <div>
+            <h2>Notifications</h2>
+            <main>
+
+            </main>
+        </div>
+    );
+}
